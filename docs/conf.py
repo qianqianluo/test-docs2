f5120981c2b8146179789bc0f2263addec280318
Qian Luo <qianqianluo@gmail.com>
2022-05-13 11:27:12 -0700

    changed title page in .pu

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ author = 'Tigase, Inc.'
 # The full version, including alpha/beta/rc tags
 release = '0.1'
 
-
+html_title = 'Tigase-Server Documentation---5/13/2022'
 # -- General configuration ---------------------------------------------------
 
 # Add any Sphinx extension module names here, as strings. They can be
